e4322871a783bde635ed1f8627f0b102639d29bd
Tino Weinkauf <weinkauf@kth.se>
2019-06-16 18:44:04 +0200

    TopoVis: Colors and other stuff

--- topovis/topologytoolkit/src/processors/morsesmalecomplextomesh.cpp
+++ topovis/topologytoolkit/src/processors/morsesmalecomplextomesh.cpp
@@ -45,9 +45,12 @@ MorseSmaleComplexToMesh::MorseSmaleComplexToMesh()
     :Processor()
     ,portInMSComplex("InMSComplex")
     ,portOutMesh("OutMesh")
+    ,propColors("Colors", "Colors")
 {
     addPort(portInMSComplex);
     addPort(portOutMesh);
+
+    addProperty(propColors);
 }
 
 void MorseSmaleComplexToMesh::process()
@@ -58,25 +61,86 @@ void MorseSmaleComplexToMesh::process()
     auto InTriangulation = pMSCData->triangulation;
 
     //Prepare Memory
-    const size_t NumCP = pMSCData->criticalPoints.numberOfPoints;
+    const ttk::SimplexId NumCP = pMSCData->criticalPoints.numberOfPoints;
     std::vector<vec3> positions(NumCP);
     std::vector<vec4> colors(NumCP, {1.0f, 1.0f, 1.0f, 1.0f});
     std::vector<float> radius(NumCP, 1.0f);
     //std::fill(radius.begin(), radius.begin() + tree->getNumberOfNodes(), sphereRadius_.get());
 
-    //Add critical points
-    for (ttk::ftm::idNode i = 0; i < NumCP; i++)
+    //Add critical points with their color
+    for (ttk::SimplexId i = 0; i < NumCP; i++)
     {
         positions[i].x = pMSCData->criticalPoints.points[3*i];
         positions[i].y = pMSCData->criticalPoints.points[3*i + 1];
         positions[i].z = pMSCData->criticalPoints.points[3*i + 2];
-        //colors.push_back(up && down ? saddleColor_ : (up ? localMaximaColor_ : localMinimaColor_));
+
+        switch (pMSCData->criticalPoints.points_cellDimensions[i])
+        {
+            case 0:
+            {
+                colors[i] = propColors.localMinima_.get();
+                //LogInfo("Min");
+                break;
+            }
+            case 1:
+            {
+                colors[i] = propColors.saddle_.get();
+                //LogInfo("Saddle");
+                break;
+            }
+            case 2:
+            {
+                if (pMSCData->triangulation->getTriangulation().getDimensionality() == 2)
+                {
+                    colors[i] = propColors.localMaxima_.get();
+                    //LogInfo("Max");
+                }
+                else
+                {
+                    colors[i] = propColors.saddle_.get();
+                    //LogInfo("Saddle");
+                }
+
+                break;
+            }
+            case 3:
+            {
+                ivwAssert(pMSCData->triangulation->getTriangulation().getDimensionality() == 3, "A critical point from a cell type of 3 can only appear in 3D or higher dimensional spaces.");
+                colors[i] = propColors.localMaxima_.get();
+                break;
+            }
+        }
+    }
+
+    //Add the separatrices
+    ttk::SimplexId NumSepPoints = pMSCData->separatrices.numberOfPoints;
+    positions.reserve(positions.size() + NumSepPoints);
+    colors.reserve(colors.size() + NumSepPoints);
+    radius.reserve(radius.size() + NumSepPoints);
+    ttk::SimplexId idPrev = positions.size();
+    for (ttk::SimplexId i = 0; i < NumSepPoints; i++)
+    {
+        positions.push_back({pMSCData->separatrices.points[3*i],
+                             pMSCData->separatrices.points[3*i + 1],
+                             pMSCData->separatrices.points[3*i + 2]});
+        colors.push_back({1,1,1,1});
+        radius.push_back(1);
+
+        //LogInfo(pMSCData->separatrices.cells_separatrixIds[i]);
     }
 
     auto mesh = std::make_shared<Mesh>(DrawType::Points, ConnectivityType::None);
     mesh->addBuffer(BufferType::PositionAttrib, util::makeBuffer(std::move(positions)));
     mesh->addBuffer(BufferType::ColorAttrib, util::makeBuffer(std::move(colors)));
     mesh->addBuffer(BufferType::RadiiAttrib, util::makeBuffer(std::move(radius)));
+    // - CP
+    std::vector<uint32_t> CPIndices(NumCP);
+    std::iota(CPIndices.begin(), CPIndices.end(), 0);
+    mesh->addIndicies(Mesh::MeshInfo(DrawType::Points, ConnectivityType::None), util::makeIndexBuffer(std::move(CPIndices)));
+    // - Sep
+    std::vector<uint32_t> SepIndices(NumSepPoints);
+    std::iota(SepIndices.begin(), SepIndices.end(), idPrev);
+    mesh->addIndicies(Mesh::MeshInfo(DrawType::Lines, ConnectivityType::None), util::makeIndexBuffer(std::move(SepIndices)));
 
     portOutMesh.setData(mesh);
 }
