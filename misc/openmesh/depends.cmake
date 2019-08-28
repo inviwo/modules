3a0fd91c28262d592b273e56e8c10f362f57b98a
= <rickard@r-englund.com>
2019-08-28 08:29:42 +0200

    Misc: Do not enable openmesh and springsytem by default

--- misc/openmesh/depends.cmake
+++ misc/openmesh/depends.cmake
@@ -2,9 +2,6 @@
 # Dependencies for current module
 # List modules on the format "Inviwo<ModuleName>Module"
 
-
-set(EnableByDefault ON)
-
 set(dependencies
     #InviwoOpenGLModule
     InviwoBaseModule  
