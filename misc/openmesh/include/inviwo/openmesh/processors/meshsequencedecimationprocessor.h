256b80261ffb7752be3d812bd109caa7a72791b7
Rickard Englund <rickard@r-englund.com>
2019-11-11 12:43:13 +0100

    OpenMesh: Compile fix

--- misc/openmesh/include/inviwo/openmesh/processors/meshsequencedecimationprocessor.h
+++ misc/openmesh/include/inviwo/openmesh/processors/meshsequencedecimationprocessor.h
@@ -33,7 +33,7 @@
 #include <inviwo/core/common/inviwo.h>
 #include <inviwo/core/processors/processor.h>
 #include <inviwo/core/properties/ordinalproperty.h>
-#include <inviwo/core/ports/imageport.h>
+#include <inviwo/core/ports/meshport.h>
 
 namespace inviwo {
 
