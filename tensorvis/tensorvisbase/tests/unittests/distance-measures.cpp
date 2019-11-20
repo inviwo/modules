7194a3350f955a9cc69bacd52158053b9de3613f
Jochen Jankowai <jochen.jankowai@liu.se>
2019-11-20 13:54:34 +0100

    Delete unit tests

--- tensorvis/tensorvisbase/tests/unittests/distance-measures.cpp
+++ tensorvis/tensorvisbase/tests/unittests/distance-measures.cpp
@@ -6,24 +6,6 @@
 #include <inviwo/tensorvisbase/util/distancemetrics.h>
 
 namespace inviwo {
-TEST(TensorUtilTests, euclideanDistanceThrow) {
-    std::vector<float> vec1;
-    std::vector<float> vec2;
-    vec1.resize(3);
-    vec2.resize(4);
-
-    EXPECT_THROW(
-        {
-            try {
-                tensorutil::euclideanDistance(vec1, vec2);
-            } catch (const std::domain_error& e) {
-                EXPECT_STREQ("Minkowski distance requires equal length vectors", e.what());
-                throw;
-            }
-        },
-        std::domain_error);
-}
-
 TEST(TensorUtilTests, euclideanDistanceSuccess) {
     std::vector<float> vec1;
     std::vector<float> vec2;
@@ -33,24 +15,6 @@ TEST(TensorUtilTests, euclideanDistanceSuccess) {
     std::iota(vec2.begin(), vec2.end(), 1);
 
     EXPECT_EQ(std::sqrt(3.0f), tensorutil::euclideanDistance(vec1, vec2));
-}
-
-TEST(TensorUtilTests, squaredSumDistanceThrow) {
-    std::vector<float> vec1;
-    std::vector<float> vec2;
-    vec1.resize(3);
-    vec2.resize(4);
-
-    EXPECT_THROW(
-        {
-            try {
-                tensorutil::squaredSumDistance(vec1, vec2);
-            } catch (const std::domain_error& e) {
-                EXPECT_STREQ("Squared sum distance requires equal length vectors", e.what());
-                throw;
-            }
-        },
-        std::domain_error);
 }
 
 TEST(TensorUtilTests, squaredSumDistanceSuccess) {
