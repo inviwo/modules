dd9b09a013eef6fe49723cd03ed0c7905f821633
Rickard Englund <rickard@r-englund.com>
2019-12-20 14:58:12 +0100

    NanoVGPy: Added missing expose of deactivate

--- misc/nanovgpy/src/interface/nanovgpyinterface.cpp
+++ misc/nanovgpy/src/interface/nanovgpyinterface.cpp
@@ -79,7 +79,8 @@ void init(py::module ivwmodule, InviwoApplication *app) {
                           self->activate(dimensions, pixelRatio);
                       },
                       py::arg("dimensions"), py::arg("pixelRatio") = 1.0f);
-
+    
+    nanoVGContext.def("deactivate", &NanoVGContext::deactivate);
     nanoVGContext.def("cancel", &NanoVGContext::cancel);
 
     nanoVGContext.def("save", &NanoVGContext::save);
