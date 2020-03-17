398b0d1fce24ca9cc4952df34794452f7ab82e34
Martin Falk <martin.falk@liu.se>
2020-03-17 16:13:02 +0100

    MolVis: added RasmolCPKnew color scheme

--- molvis/molvisbase/include/inviwo/molvisbase/util/molvisutils.h
+++ molvis/molvisbase/include/inviwo/molvisbase/util/molvisutils.h
@@ -84,7 +84,8 @@ static constexpr const char* names[num_elements] = {
     "Curium",     "Berkelium",    "Californium",   "Einsteinium", "Fermium",     "Mendelevium",
     "Nobelium",   "Lawrencium",   "Rutherfordium", "Dubnium",     "Seaborgium",  "Bohrium",
     "Hassium",    "Meitnerium",   "Darmstadtium",  "Roentgenium", "Copernicium", "Nihonium",
-    "Flerovium",  "Moscovium",    "Livermorium",   "Tennessine",  "Oganesson"};
+    "Flerovium",  "Moscovium",    "Livermorium",   "Tennessine",  "Oganesson",
+};
 
 static constexpr const char* symbols[num_elements] = {
     "Xx", "H",  "He", "Li", "Be", "B",  "C",  "N",  "O",  "F",  "Ne", "Na", "Mg", "Al", "Si",
@@ -94,7 +95,8 @@ static constexpr const char* symbols[num_elements] = {
     "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W",
     "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac",
     "Th", "Pa", "U",  "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf",
-    "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og"};
+    "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og",
+};
 
 // http://dx.doi.org/10.1039/b801115j
 static constexpr double covalent_radii[num_elements] = {
@@ -105,7 +107,8 @@ static constexpr double covalent_radii[num_elements] = {
     2.01, 1.99, 1.98, 1.98, 1.96, 1.94, 1.92, 1.92, 1.89, 1.90, 1.87, 1.87, 1.75, 1.70, 1.62,
     1.51, 1.44, 1.41, 1.36, 1.36, 1.32, 1.45, 1.46, 1.48, 1.40, 1.50, 1.50, 2.60, 2.21, 2.15,
     2.06, 2.00, 1.96, 1.90, 1.87, 1.80, 1.69, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60,
-    1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60};
+    1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60, 1.60,
+};
 
 // https://dx.doi.org/10.1021/jp8111556
 static constexpr double vdw_radii[num_elements] = {
@@ -116,7 +119,8 @@ static constexpr double vdw_radii[num_elements] = {
     2.45, 2.43, 2.42, 2.40, 2.38, 2.37, 2.35, 2.33, 2.32, 2.30, 2.28, 2.27, 2.25, 2.20, 2.10,
     2.05, 2.00, 2.00, 2.05, 2.10, 2.05, 1.96, 2.02, 2.07, 1.97, 2.02, 2.20, 3.48, 2.83, 2.00,
     2.40, 2.00, 2.30, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00,
-    2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00};
+    2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00, 2.00,
+};
 
 // http://chemistry.wikia.com/wiki/List_of_elements_by_atomic_mass
 // last element padded with 295
@@ -135,10 +139,12 @@ static constexpr double atomic_mass[num_elements] = {
     226,     227,        231.03588, 232.03806,   237,      238.02891,  243,     244,
     247,     247,        251,       252,         257,      258,        259,     262,
     261,     262,        266,       264,         277,      268,        271,     272,
-    285,     284,        289,       288,         292,      294,        295};
+    285,     284,        289,       288,         292,      294,        295,
+};
 
 // http://jmol.sourceforge.net/jscolors/ (stored as RGBA from least to most significant bit)
-static constexpr unsigned int colors[num_elements] = {
+// Rasmol colors
+static constexpr unsigned int colorsRasmol[num_elements] = {
     0xFFFF00FF, 0xFFFFFFFF, 0xFFFFFFD9, 0xFF2222B2, 0xFF00FFC2, 0xFFB5B5FF, 0xFFB0B0B0, 0xFFFF8F8F,
     0xFF0000F0, 0xFF50E090, 0xFFF5E3B3, 0xFFF25CAB, 0xFF00FF8A, 0xFF908080, 0xFFA0C8F0, 0xFF00A5FF,
     0xFF32C8FF, 0xFF1FF01F, 0xFFE3D180, 0xFFD4408F, 0xFF908080, 0xFFE6E6E6, 0xFF908080, 0xFFABA6A6,
@@ -153,7 +159,27 @@ static constexpr unsigned int colors[num_elements] = {
     0xFF007D00, 0xFFFAAB70, 0xFFFFBA00, 0xFFFFA100, 0xFFFF8F00, 0xFFFF8000, 0xFFFF6B00, 0xFFF25C54,
     0xFFE35C78, 0xFFE34F8A, 0xFFD436A1, 0xFFD41FB3, 0xFFBA1FB3, 0xFFA60DB3, 0xFF870DBD, 0xFF6600C7,
     0xFF5900CC, 0xFF4F00D1, 0xFF4500D9, 0xFF3800E0, 0xFF2E00E6, 0xFF2600EB, 0xFF2200F0, 0xFF2000F6,
-    0xFF1E00F8, 0xFF1C00FA, 0xFF1A00FC, 0xFF1800FD, 0xFF1600FE, 0xFF1400FF, 0xFF1200FF};
+    0xFF1E00F8, 0xFF1C00FA, 0xFF1A00FC, 0xFF1800FD, 0xFF1600FE, 0xFF1400FF, 0xFF1200FF,
+};
+
+// Rasmol CPKnew colors
+static constexpr unsigned int colorsRasmolCPKnew[num_elements] = {
+    0xFFFF00FF, 0xFFFFFFFF, 0xFFFFFFD9, 0xFF2121B2, 0xFF00FFC2, 0xFFB5B5FF, 0xFFD3D3D3, 0xFFE6CE87,
+    0xFF0000FF, 0xFF50E090, 0xFFF5E3B3, 0xFFF25CAB, 0xFF00FF8A, 0xFF696969, 0xFFA0C8F0, 0xFF00AAFF,
+    0xFF00FFFF, 0xFF1FF01F, 0xFFE3D180, 0xFFD4408F, 0xFF696969, 0xFFE6E6E6, 0xFF696969, 0xFFABA6A6,
+    0xFF696969, 0xFF696969, 0xFF00AAFF, 0xFFA090F0, 0xFF282880, 0xFF282880, 0xFF282880, 0xFF8F8FC2,
+    0xFF8F8F66, 0xFFE380BD, 0xFF00A1FF, 0xFF282880, 0xFFD1B85C, 0xFFB02E70, 0xFF00FF00, 0xFFFFFF94,
+    0xFFE0E094, 0xFFC9C273, 0xFFB5B554, 0xFF9E9E3B, 0xFF8F8F24, 0xFF8C7D0A, 0xFF856900, 0xFF696969,
+    0xFF8FD9FF, 0xFF7375A6, 0xFF808066, 0xFFB5639E, 0xFF007AD4, 0xFF940094, 0xFFB09E42, 0xFF8F1757,
+    0xFF00AAFF, 0xFFFFD470, 0xFFC7FFFF, 0xFFC7FFD9, 0xFFC7FFC7, 0xFFC7FFA3, 0xFFC7FF8F, 0xFFC7FF61,
+    0xFFC7FF45, 0xFFC7FF30, 0xFFC7FF1F, 0xFF9CFF00, 0xFF75E600, 0xFF52D400, 0xFF38BF00, 0xFF24AB00,
+    0xFFFFC24D, 0xFFFFA64D, 0xFFD69421, 0xFFAB7D26, 0xFF966626, 0xFF875417, 0xFFE0D0D0, 0xFF23D1FF,
+    0xFFD0B8B8, 0xFF4D54A6, 0xFF615957, 0xFFB54F9E, 0xFF005CAB, 0xFF454F75, 0xFF968242, 0xFF660042,
+    0xFF007D00, 0xFFFAAB70, 0xFFFFBA00, 0xFFFFA100, 0xFFFF8F00, 0xFFFF8000, 0xFFFF6B00, 0xFFF25C54,
+    0xFFE35C78, 0xFFE34F8A, 0xFFD436A1, 0xFFD41FB3, 0xFFBA1FB3, 0xFFA60DB3, 0xFF870DBD, 0xFF6600C7,
+    0xFF5900CC, 0xFF4F00D1, 0xFF4500D9, 0xFF3800E0, 0xFF2E00E6, 0xFF2600EB, 0xFF2200F0, 0xFF2000F6,
+    0xFF1E00F8, 0xFF1C00FA, 0xFF1A00FC, 0xFF1800FD, 0xFF1600FE, 0xFF1400FF, 0xFF1200FF,
+};
 
 }  // namespace detail
 
@@ -162,7 +188,7 @@ constexpr Element element(int atomicNumber) { return static_cast<Element>(atomic
 constexpr const char* name(Element symbol) { return detail::names[(int)symbol]; }
 constexpr const char* symbol(Element symbol) { return detail::symbols[(int)symbol]; }
 constexpr vec4 color(Element symbol) {
-    unsigned int c = detail::colors[(int)symbol];
+    unsigned int c = detail::colorsRasmolCPKnew[static_cast<int>(symbol)];
     return vec4{(c >> 0) & 0xff, (c >> 8) & 0xff, (c >> 16) & 0xff, (c >> 24) & 0xff} / 255.0f;
 }
 constexpr double vdwRadius(Element symbol) { return detail::vdw_radii[(int)symbol]; }
