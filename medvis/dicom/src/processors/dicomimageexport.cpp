d25cc7543c7acd5d57d9f3073ea86a9019aeabee
VisComKreiser <julian.kreiser@uni-ulm.de>
2020-05-31 01:15:57 +0200

    updated Tag and added attributes

--- medvis/dicom/src/processors/dicomimageexport.cpp
+++ medvis/dicom/src/processors/dicomimageexport.cpp
@@ -31,6 +31,8 @@
 #include <inviwo/core/datastructures/image/layerram.h>
 
 #include <string>
+#include <chrono>
+#include <thread>
 
 #include "gdcmWriter.h"
 #include "gdcmReader.h"
@@ -53,7 +55,7 @@ const ProcessorInfo DICOMImageExport::processorInfo_{
     "DICOM Image Export",           // Display name
     "Data Output",                  // Category
     CodeState::Experimental,        // Code state
-    Tags::None,                     // Tags
+    Tags::CPU,                      // Tags
 };
 const ProcessorInfo DICOMImageExport::getProcessorInfo() const { return processorInfo_; }
 
@@ -65,6 +67,8 @@ DICOMImageExport::DICOMImageExport()
 }
 
 void DICOMImageExport::process() {
+    const auto t0 = std::chrono::steady_clock::now();
+
     const auto& img = inport_.getData();
     const auto& color_layer_ram = img->getColorLayer(0)->getRepresentation<LayerRAM>();
     const auto color_data = static_cast<const char*>(color_layer_ram->getData());
@@ -102,7 +106,7 @@ void DICOMImageExport::process() {
     gdcm::Attribute<0x0010, 0x0030> attr_patient_dob = {"attr_patient_dob"};
     gdcm::Attribute<0x0010, 0x0040> attr_patient_sex = {"attr_patient_sex"};
     gdcm::Attribute<0x0020, 0x0010> attr_study_id = {"attr_study_id"};
-    gdcm::Attribute<0x0020, 0x0011> attr_series_number = {static_cast<int32_t>(0)};
+    gdcm::Attribute<0x0020, 0x0011> attr_series_number = {static_cast<int32_t>(42)};
     gdcm::Attribute<0x0020, 0x0020> attr_patient_orientation = {"attr_patient_orientation"};
 
     // additional attributes
@@ -115,6 +119,44 @@ void DICOMImageExport::process() {
     gdcm::Attribute<0x0028, 0x0103> attr_pixel_representation = {static_cast<uint16_t>(0x0001)};
     gdcm::Attribute<0x0028, 0x0034> attr_pixel_aspect_ratio = {static_cast<uint16_t>(1),
                                                                static_cast<uint16_t>(1)};
+    gdcm::Attribute<0x0028, 0x0004> attr_photometric_interpretation = {"RGB"};
+    gdcm::Attribute<0x0028, 0x0006> attr_planar_configuration = {static_cast<uint16_t>(0)};
+
+    // x-ray data frame attributes:
+    // Image Columns	        0x0028, 0x0010
+    // Image Rows	            0x0028, 0x0011
+    // Primary angulation	    0x0018, 0x1510
+    // Secondary angulation	    0x0018, 0x1511
+    // Table position in vertical (anteriour/posteriour) direction (z)  0x2003, 0x102E	Subtag: 0x300A, 0x0128
+    // Table position in longitudinal (head-feet) direction (y)         0x2003, 0x102E	Subtag: 0x300A, 0x0129
+    // Table position in lateral (right-left) direction (x)	            0x2003, 0x102E	Subtag: 0x300A, 0x012A
+    // Source-to-image distance (SID)	    0x0018, 0x1110
+    // Zoom field (FD)	                    0x2003, 0x1003 or 0x2003, 0x1010
+    // Pixel Spacing (x/y)	                0x0018, 0x1164
+    // Source-to-patient distance (SPD)	    0x0018, 0x1111
+    // Cine frame rate	                    0x0018, 0x0040
+    // ECG frame rate	                    0x5000, 0x0114
+    // ECG number of points	                0x5000, 0x0010
+    // ECG signal	                        0x5000, 0x3000
+    /*
+    {
+        gdcm::Attribute<0x0028, 0x0010> attr_image_columns = {};
+        gdcm::Attribute<0x0028, 0x0011> attr_image_rows = {};
+        gdcm::Attribute<0x0018, 0x1510> attr_primary_angulation = {};
+        gdcm::Attribute<0x0018, 0x1511> attr_secondary_angulation = {};
+        gdcm::Attribute<0x2003, 0x102E> attr_table_pos_vertical = {};      // subtag: 0x300A, 0x0128
+        gdcm::Attribute<0x2003, 0x102E> attr_table_pos_longitudinal = {};  // subtag: 0x300A, 0x0129
+        gdcm::Attribute<0x2003, 0x102E> attr_table_pos_lateral = {};       // subtag: 0x300A, 0x012A
+        gdcm::Attribute<0x0018, 0x1110> attr_source_to_image_distance = {};
+        gdcm::Attribute<0x2003, 0x1003> attr_zoom_field = {};  // or: 0x2003, 0x1010
+        gdcm::Attribute<0x0018, 0x1164> attr_pixel_spacing = {};
+        gdcm::Attribute<0x0018, 0x1111> attr_source_to_patient_distance = {};
+        gdcm::Attribute<0x0018, 0x0040> attr_cine_frame_rate = {};
+        gdcm::Attribute<0x5000, 0x0114> attr_ecg_frame_rate = {};
+        gdcm::Attribute<0x5000, 0x0010> attr_ecg_number_of_points = {};
+        gdcm::Attribute<0x5000, 0x3000> attr_ecg_signal = {};
+    }
+    */
 
     // manually build DataElements
     gdcm::DataElement datel_pixel_data(gdcm::Tag(0x7fe0, 0x0010));
@@ -147,6 +189,9 @@ void DICOMImageExport::process() {
     data_set.Insert(attr_high_bit.GetAsDataElement());
     data_set.Insert(attr_pixel_representation.GetAsDataElement());
     data_set.Insert(attr_pixel_aspect_ratio.GetAsDataElement());
+    data_set.Insert(attr_photometric_interpretation.GetAsDataElement());
+    data_set.Insert(attr_planar_configuration.GetAsDataElement());
+
     data_set.Insert(datel_pixel_data);
 
     // print data set and meta info
@@ -160,6 +205,10 @@ void DICOMImageExport::process() {
     if (!writer.Write()) {
         LogWarn("could not write: " << filename);
     }
+
+    const auto t1 = std::chrono::steady_clock::now();
+    LogInfo(
+        "dicom export: " << std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count());
 }
 
 }  // namespace inviwo
