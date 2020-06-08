12f53353b563e801c3bd8cdc40eec05bffdfc378
Anke <anke.friederici@gmx.de>
2020-06-08 20:29:33 +0200

    Allowing to combine variables and slice

--- misc/netcdf/python/processors/netcdfsource.py
+++ misc/netcdf/python/processors/netcdfsource.py
@@ -1,32 +1,37 @@
 # Name: NetCDFSource
 
 import inviwopy as ivw
-from inviwopy.properties import IntVec3Property, FileProperty, OptionPropertyString, ButtonProperty
+from inviwopy.properties import IntVec3Property, FileProperty, OptionPropertyString, ButtonProperty, BoolProperty, CompositeProperty, DoubleMinMaxProperty, IntMinMaxProperty
 from inviwopy.glm import dvec2, mat4, vec4
 import netcdfutils
+import genericnetcdfsource
 
-# # import functools
-# # import math
 import numpy
 from pathlib import Path
 from netCDF4 import Dataset
 
 
-class NetCDFSource(ivw.Processor):
+class NetCDFSource(ivw.Processor):  # GenericNetCDFSource):
 
     def __init__(self, id, name):
+        self.outputDimension = 3
         ivw.Processor.__init__(self, id, name)
 
         self.data = []
-        self.volumeOutport = ivw.data.VolumeSequenceOutport("data3D")
+        # self.variableSizes = []
+        self.volumeOutport = ivw.data.VolumeOutport("data3D")
         self.addOutport(self.volumeOutport, owner=False)
 
         self.displayInfo = ButtonProperty("displayInfo", "Log File Info")
 
         self.filePath = ivw.properties.FileProperty("filepath", "NetCDF Path")
-        self.variable = OptionPropertyString("variable", "Variable")
-        # self.dimensionIds = [OptionPropertyString("dimX", "Dim X"), OptionPropertyString(
-        #     "dimY", "Dim Y"), OptionPropertyString("dimZ", "Dim Z")]
+        self.variables = CompositeProperty("variables", "Exported Variables")
+        self.dimensions = CompositeProperty(
+            "dimensions", "Restrict Dimensions")
+        self.adjustDimensionsForStaggered = BoolProperty(
+            'adjustForStaggered', 'Adjust for staggered climate grid', True)
+        self.toFloat = BoolProperty(
+            'toFloat', 'Convert to float', True)
 
         self.triggerReload = ButtonProperty("reload", "Reload")
 
@@ -35,15 +40,20 @@ class NetCDFSource(ivw.Processor):
 
         self.addProperty(self.displayInfo)
         self.addProperty(self.filePath)
-        self.addProperty(self.variable)
-        # for dimId in self.dimensionIds:
-        #     self.addProperty(dimId)
+        self.addProperty(self.variables)
+        self.addProperty(self.toFloat)
+        self.addProperty(self.dimensions)
+        self.addProperty(self.adjustDimensionsForStaggered)
         self.addProperty(self.triggerReload)
 
-        # self.dataRange = ivw.properties.DoubleMinMaxProperty(
-        #     "dataRange", "Data Range", 0.0, 0.0, -1.70e308, 1.79e308)
-        # self.addProperty(self.dataRange)
-        # self.dataRange.semantics = ivw.properties.PropertySemantics("Text")
+        self.overwriteDataRange = BoolProperty(
+            "overwriteDataRange", "Overwrite Data Range", False)
+
+        self.dataRange = DoubleMinMaxProperty(
+            "dataRange", "Data Range", 0.0, 0.0, -1.70e308, 1.79e308)
+        self.addProperty(self.overwriteDataRange)
+        self.addProperty(self.dataRange)
+        self.dataRange.semantics = ivw.properties.PropertySemantics("Text")
         # self.dataRange.readOnly = True
 
     @staticmethod
@@ -65,46 +75,86 @@ class NetCDFSource(ivw.Processor):
         pass
 
     def process(self):
-        print("Process called")
+        # print("Process called")
 
         if len(self.filePath.value) == 0 or not Path(self.filePath.value).exists():
             print("No valid path given")
             return
 
         with Dataset(self.filePath.value, "r", format="NETCDF4") as nc:
-            if self.filePath.isModified or self.variable.isModified:
-                # print("Something happened!")
 
-                # Update variables.
-                if self.filePath.isModified:
-                    print("Path is modified")
-                    # Save last selected option to set again.
-                    lastSelected = ""
-                    if len(self.variable.values) > 0:
-                        lastSelected = self.variable.value
+            # Update variables.
+            if self.filePath.isModified:
+                # Keep variables as is iff the new data has the same ones.
+                reloadVariables = True
+                if self.variables.size() > 0:
+                    reloadVariables = False
+                    for variable in self.variables.properties:
+                        if variable not in nc.variables:
+                            reloadVariables = True
+                            break
 
-                    # Update all variables as options.
-                    for option in self.variable.values:
-                        self.variable.removeOption(option)
+                if reloadVariables:
+                    while self.variables.size() > 0:
+                        self.variables.removeProperty(
+                            self.variables.properties[self.variables.size()-1])
+
+                    while self.dimensions.size() > 0:
+                        self.dimensions.removeProperty(
+                            self.dimensions.properties[self.dimensions.size()-1])
+
                     for variable in nc.variables:
-                        self.variable.addOption(
-                            str(variable), str(variable), str(variable))
-                        if variable == lastSelected:
-                            self.variable.value = variable
+                        # At least n dimensions needed for an nD output
+                        if len(nc.variables[variable].dimensions) < self.outputDimension:
+                            continue
+                        self.variables.addProperty(
+                            BoolProperty(str(variable), str(variable), False), True)
 
-    #             for dimId in self.dimensionIds:
-    #                 # Save last selected option to set again.
-    #                 lastSelected = ""
-    #                 if len(dimId.values) > 0:
-    #                     lastSelected = dimId.value
+            if self.variables.isModified:
+                selectedVarDims = []
+                numComponents = 0
+                for varProp in self.variables.properties:
+                    if not varProp.value:
+                        continue
 
-    #                 # Update all dimensions as options.
-    #                 for option in dimId.values:
-    #                     dimId.removeOption(option)
-    #                 for dim in nc.dimensions:
-    #                     dimId.addOption(str(dim), str(dim), str(dim))
-    #                     if dim == lastSelected:
-    #                         dimId.value = dim
+                    numVarComps = numpy.amax(
+                        1, nc.variables[varProp.identifier].datatype.ndim)
+                    numComponents += numVarComps
+
+                    # Collect all dimension names in one list
+                    for dim in nc.variables[varProp.identifier].get_dims():
+                        adjustedDim = self.adjustForStaggered(dim.name)
+                        if adjustedDim not in selectedVarDims:
+                            selectedVarDims.append(adjustedDim)
+
+                            if not next((x for x in self.dimensions.properties if x.identifier == adjustedDim), None):
+                                self.dimensions.addProperty(IntMinMaxProperty(
+                                    adjustedDim, adjustedDim + ' Range', 0, len(dim)-1, 0, len(dim)-1), True)
+
+                print("Num components: " + str(numComponents) +
+                      "\n\tselectedVarDims:")
+                print(selectedVarDims)
+                for varProp in self.variables.properties:
+                    if varProp.value:
+                        continue
+
+                    # Check whether there is still space to add this variable
+                    # and it stretches over all dimensions  the others do
+                    couldBeAdded = True
+                    ncVar = nc.variables[varProp.identifier]
+                    numVarComps = numpy.amax(1, ncVar.datatype.ndim)
+                    if numVarComps > 4 - numComponents:
+                        couldBeAdded = False
+
+                    varDims = map(self.adjustForStaggered, ncVar.dimensions)
+
+                    for dim in selectedVarDims:
+                        adjustedDim = self.adjustForStaggered(dim)
+                        if adjustedDim not in varDims:
+                            couldBeAdded = False
+                            break
+
+                    varProp.readOnly = not couldBeAdded
 
     def reloadData(self):
         if len(self.filePath.value) == 0 or not Path(self.filePath.value).exists():
@@ -113,46 +163,66 @@ class NetCDFSource(ivw.Processor):
 
         with Dataset(self.filePath.value, "r", format="NETCDF4") as nc:
             # Actually load data.
-            if len(self.variable.values) <= 0:
+            if self.variables.size() <= 0:
                 print("No known variables")
                 return
 
-            ncVar = nc.variables[self.variable.value]
-            ncDims = ncVar.get_dims()
-            # print("Var " + str(self.variable.value) +
-            #       " with dims " + str(ncDims))
-            # for dim in ncDims:
-            #     print("Dim " + str(dim) + " of size " + str(len(dim)))
+            sizeDims = []
+            dimRanges = {}
+            for dimProp in self.dimensions.properties:
+                dimRange = dimProp.value
+                dimRanges[dimProp.identifier] = dimRange
+                if dimRange.x != dimRange.y:
+                    sizeDims.append(dimRange.y - dimRange.x + 1)
 
-            dims = []
-            for dim in ncDims:
-                dims.append(len(dim))
+            if len(sizeDims) != self.outputDimension:
+                print("Wrong number of dimensions.\n\tRequire " +
+                      str(self.outputDimension) + ", selected " + str(len(sizeDims)))
+                return
 
-            # 4D data, i.e., a volume sequence?
-            if len(ncDims) == 4:
-                print("4D!")
-                timeLength = dims[0]
-                print("Time " + str(timeLength))
-                self.data = []
-                for timeStep in range(timeLength):
-                    varData = ncVar[timeStep, :, :, :]
-                    buffer = numpy.array(varData).astype('float32')
-                    # ncVar.datatype)
-                    buffer.shape = [dims[3], dims[2], dims[1]]
-                    minVal = numpy.amin(buffer)
-                    maxVal = numpy.amax(buffer)
-                    print("Min " + str(minVal) + ", max " + str(maxVal))
-                    # print(buffer.shape)
-                    volume = ivw.data.Volume(buffer)
-                    volume.dataMap.dataRange = dvec2(minVal, maxVal)
-                    volume.dataMap.valueRange = dvec2(0.0, 1000.0)
-                    volume.modelMatrix = mat4(vec4(dims[3], 0, 0, 0),
-                                              vec4(0, dims[2], 0, 0),
-                                              vec4(0, 0, dims[1], 0),
-                                              vec4(0, 0, 0, 1))
-                    self.data.append(volume)
-                self.volumeOutport.setData(ivw.data.VolumeSequence(self.data))
+            # numComponents = ()
+            varIDs = []
+            self.data = []
+            for varProp in self.variables.properties:
+                if not varProp.value:
+                    continue
+                varIDs.append(varProp.identifier)
+
+                # Single variable, simply load
+                ncVar = nc.variables[varIDs[0]]
+                ncDims = ncVar.get_dims()
+                dims = []
+                for ncDim in ncDims:
+                    propRange = dimRanges[self.adjustForStaggered(ncDim.name)]
+                    dims.append(slice(propRange.x, propRange.y+1))
+
+                varData = ncVar[tuple(dims)]
+                buffer = numpy.array(varData).astype(
+                    'float32' if self.toFloat.value else ncVar.datatype)
+
+                buffer.shape = numpy.flip([1] + sizeDims)
+                self.data.append(buffer)
+                # print("Buffer size: " + str(buffer.shape))
+
+            buffer = numpy.concatenate(self.data, axis=3)
+            print("Buffer size: " + str(buffer.shape))
+            minVal = numpy.amin(buffer)
+            maxVal = numpy.amax(buffer)
+            volume = ivw.data.Volume(buffer)
+            volume.dataMap.dataRange = dvec2(minVal, maxVal)
+            volume.dataMap.valueRange = dvec2(-1000, 1000.0)
+            volume.modelMatrix = mat4(
+                vec4(sizeDims[2], 0, 0, 0),
+                vec4(0, sizeDims[1], 0, 0),
+                vec4(0, 0, sizeDims[0], 0),
+                vec4(0, 0, 0, 1))
+        self.volumeOutport.setData(volume)
 
     def displayDataInfo(self):
         print(self.filePath.value)
         netcdfutils.printInfo(self.filePath.value)
+
+    def adjustForStaggered(self, dim):
+        if self.adjustDimensionsForStaggered.value and len(dim) == 2 and dim[1] == 'G' and (dim[0] == 'X' or dim[0] == 'Y'):
+            return str(dim[0]) + 'C'
+        return dim
