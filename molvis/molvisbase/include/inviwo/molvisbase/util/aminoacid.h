2cdc0f0632a97c408cba2d0c238e29a8a1f589d0
Martin Falk <martin.falk@liu.se>
2021-02-11 11:32:29 +0100

    MolVis: copyright update

--- molvis/molvisbase/include/inviwo/molvisbase/util/aminoacid.h
+++ molvis/molvisbase/include/inviwo/molvisbase/util/aminoacid.h
@@ -2,7 +2,7 @@
  *
  * Inviwo - Interactive Visualization Workshop
  *
- * Copyright (c) 2020 Inviwo Foundation
+ * Copyright (c) 2020-2021 Inviwo Foundation
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -68,7 +68,7 @@ constexpr std::array<std::string_view, num_aminoacids> names = {
     "Glutamine",  "Glutamic acid", "Glycine",       "Histidine",      "Isoleucine",    "Leucine",
     "Lysine",     "Methionine",    "Phenylalanine", "Proline",        "Serine",        "Threonine",
     "Tryptophan", "Tyrosine",      "Valine",        "Selenocysteine", "Pyrrolysine",   
-    "Asparagine or Aspartic acid", "Glutamine or Glutamic acid",  "Leucine or Isoleucine",
+    "AsparagineÂ or Aspartic acid", "GlutamineÂ or Glutamic acid",  "LeucineÂ or Isoleucine",
 };
 
 constexpr std::array<char, num_aminoacids> letter = {
