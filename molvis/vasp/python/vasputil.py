ae8edefb026cc84792bad014434ebc42c73fbe9a
Signe Sidwall Thygesen <signe.sidwall.thygesen@liu.se>
2022-01-11 15:33:00 +0100

    Vasp: use ivwmolvis instead of atomdata

--- molvis/vasp/python/vasputil.py
+++ molvis/vasp/python/vasputil.py
@@ -1,6 +1,35 @@
+#################################################################################
+ #
+ # Inviwo - Interactive Visualization Workshop
+ #
+ # Copyright (c) 2020-2021 Inviwo Foundation
+ # All rights reserved.
+ #
+ # Redistribution and use in source and binary forms, with or without
+ # modification, are permitted provided that the following conditions are met:
+ #
+ # 1. Redistributions of source code must retain the above copyright notice, this
+ # list of conditions and the following disclaimer.
+ # 2. Redistributions in binary form must reproduce the above copyright notice,
+ # this list of conditions and the following disclaimer in the documentation
+ # and/or other materials provided with the distribution.
+ #
+ # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ # DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ # ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ # (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ # LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ # ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ #
+ #################################################################################
+
 import inviwopy as ivw
 import ivwdataframe as df
-import atomdata
+import ivwmolvis
 
 import functools
 import itertools
@@ -87,8 +116,9 @@ def createMesh(pos, elemtype, basis, offset, pm, margin):
     pm.resize(len(elemtype))
 
     for i, p in enumerate(pos):
-        c = atomdata.color(elemtype[i])
-        r = atomdata.radius(elemtype[i])
+        element = ivwmolvis.atomicelement.fromAbbr(elemtype[i])
+        c = numpy.array(ivwmolvis.atomicelement.color(element))
+        r = ivwmolvis.atomicelement.vdwRadius(element)
         pi = pm.pickingId(i)
 
         def addVertex(vertexpos):
