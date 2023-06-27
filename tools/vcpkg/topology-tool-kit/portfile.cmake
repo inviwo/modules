50ea4f84f2e540c4cd84176fef9a740ddb6c2a09
Martin Falk <martin.falk@liu.se>
2023-06-27 17:04:17 +0200

    TTK: vcpkg fix

--- tools/vcpkg/topology-tool-kit/portfile.cmake
+++ tools/vcpkg/topology-tool-kit/portfile.cmake
@@ -4,6 +4,10 @@ vcpkg_from_github(
     REF ${VERSION} 
     SHA512 811092769fd1c5dfbd230998d544607e8644a69b4be3ff433d171f255dd29cf93746bfa39d9cd8f1e53ab48aaf0714c18fe68a71818eaeeaa998ff36b2627c89
     HEAD_REF master
+    PATCHES
+        "config-cmake.patch"
+        "vtk-module.patch"
+        "vtk-module-cmake.patch"
 )
 
 vcpkg_configure_cmake(
@@ -29,6 +33,7 @@ vcpkg_fixup_cmake_targets(
 )
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 file(
     INSTALL ${SOURCE_PATH}/LICENSE 
