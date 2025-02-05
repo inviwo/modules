5e58ab61bb65a38d0a16c8859b8b198002c1b48c
Peter Steneteg <peter@steneteg.se>
2025-02-05 16:10:05 +0100

    Various: minor fixes

--- misc/nanovgutils/include/inviwo/nanovgutils/nanovgcontext.h
+++ misc/nanovgutils/include/inviwo/nanovgutils/nanovgcontext.h
@@ -320,7 +320,7 @@ private:
 
 template <>
 struct EnumTraits<NanoVGContext::Alignment> {
-    static std::string_view name() { return "Text Alignment"; }
+    static constexpr std::string_view name() { return "Text Alignment"; }
 };
 
 }  // namespace inviwo
