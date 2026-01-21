b512f0012b8e480a6224eefa9a1bdf000d4b5ae8
Peter Steneteg <peter@steneteg.se>
2026-01-21 16:53:32 +0100

    OrTools: new module for integration of OR-Tools - Google Optimization Tools (https://github.com/google/or-tools)

---
+++ misc/ortools/src/ortoolsmodule.cpp
@@ -0,0 +1,74 @@
+/*********************************************************************************
+ *
+ * Inviwo - Interactive Visualization Workshop
+ *
+ * Copyright (c) 2026 Inviwo Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************************/
+
+#include <inviwo/ortools/ortoolsmodule.h>
+#include <inviwo/ortools/processors/hungarian.h>
+
+namespace inviwo {
+
+ortoolsModule::ortoolsModule(InviwoApplication* app) : InviwoModule(app, "ortools") {
+    // Add a directory to the search path of the Shadermanager
+    // ShaderManager::getPtr()->addShaderSearchPath(getPath(ModulePath::GLSL));
+
+    // Register objects that can be shared with the rest of inviwo here:
+
+    // Processors
+    registerProcessor<Hungarian>();
+    // registerProcessor<ortoolsProcessor>();
+
+    // Properties
+    // registerProperty<ortoolsProperty>();
+
+    // Readers and writes
+    // registerDataReader(std::make_unique<ortoolsReader>());
+    // registerDataWriter(std::make_unique<ortoolsWriter>());
+
+    // Data converters
+    // registerRepresentationConverter(std::make_unique<ortoolsDisk2RAMConverter>());
+
+    // Ports
+    // registerPort<ortoolsOutport>();
+    // registerPort<ortoolsInport>();
+
+    // PropertyWidgets
+    // registerPropertyWidget<ortoolsPropertyWidget, ortoolsProperty>("Default");
+
+    // Dialogs
+    // registerDialog<ortoolsDialog>(ortoolsOutport);
+
+    // Other things
+    // registerCapabilities(std::make_unique<ortoolsCapabilities>());
+    // registerSettings(std::make_unique<ortoolsSettings>());
+    // registerMetaData(std::make_unique<ortoolsMetaData>());
+    // registerPortInspector("ortoolsOutport", "path/workspace.inv");
+    // registerProcessorWidget(std::string processorClassName, std::unique_ptr<ProcessorWidget> processorWidget); 
+    // registerDrawer(std::make_unique_ptr<ortoolsDrawer>());
+}
+
+}  // namespace inviwo
